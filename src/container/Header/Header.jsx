57442db86c441d822de610b56ce214196c847aff
songhee24 <akaratel61@gmail.com>
2023-07-07 16:29:21 +0600

    Header layout

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import "./Header.css";
+import SubHeading from "../../components/SubHeading/SubHeading";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper-info">
+      <SubHeading />
+    </div>
+
+    <div className="app__wrapper-img"></div>
   </div>
 );
 

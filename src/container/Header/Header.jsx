2bfd0d8c4e5ac981d9fafc840bf1611f90e0363c
songhee24 <akaratel61@gmail.com>
2023-07-07 16:56:19 +0600

    Subheading component #2

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,6 +8,12 @@ const Header = () => (
     <div className="app__wrapper-info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus
+      </p>
+      <button className="custom__button">Explore Menu</button>
     </div>
 
     <div className="app__wrapper-img"></div>

aebc7ae2675a8649bc9984c7dec0488d9a63273e
songhee24 <akaratel61@gmail.com>
2023-07-07 17:16:16 +0600

    styling about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -29,12 +29,12 @@ const AboutUs = () => (
       </div>
 
       <div className="app__aboutus-content">
-        <h1 className="headtext__cormorant">About Us</h1>
+        <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="left spoon" className="spoon__img img" />
         <p className="p__opensans">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
-          adipiscing ultrices vulputate posuere tristique. In sed odio nec
-          aliquet eu proin mauris et.
+          Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
+          Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
+          odio nec aliquet.
         </p>
         <button type="button" className="custom__button">
           Know More

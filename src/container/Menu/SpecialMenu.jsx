3884ede4f46ce004d7424340d45df7b581f1c917
songhee24 <akaratel61@gmail.com>
2023-07-07 18:18:44 +0600

    Begin to special menu section #2

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,7 +7,6 @@ import "./SpecialMenu.css";
 const SpecialMenu = () => {
   return (
     <div className="app__specialMenu flex__center section__padding" id="menu">
-      <h1>Hellooo</h1>
       <div className="app__specialMenu-title">
         <SubHeading title={"Menu that fits you palatte"} />
         <h1 className="headtext__cormorant">Today’s Special</h1>
@@ -20,6 +19,10 @@ const SpecialMenu = () => {
               return <p key={index}>{wine.title}</p>;
             })}
           </div>
+        </div>
+
+        <div className="app__specialMenu-menu_img">
+          <img src={images.menu} alt="menu img" />
         </div>
       </div>
     </div>

b15ddc98925f98cedc2791259c713d7fbea12615
songhee24 <akaratel61@gmail.com>
2023-07-07 16:43:19 +0600

    Subheading component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,7 +6,8 @@ import SubHeading from "../../components/SubHeading/SubHeading";
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper-info">
-      <SubHeading />
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
     </div>
 
     <div className="app__wrapper-img"></div>

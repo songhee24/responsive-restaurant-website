cfd2425fe7c86b4b8134851a897707ec6e3487c4
songhee24 <akaratel61@gmail.com>
2023-07-10 20:59:27 +0600

    done Chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -32,6 +32,7 @@ const Chef = () => {
         <div className="app__chef-sign">
           <p>Kevin Luo</p>
           <p className="p__opensans">Chef & Founder</p>
+          <img src={images.sign} alt="sign" />
         </div>
       </div>
     </div>

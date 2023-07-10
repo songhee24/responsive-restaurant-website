3d59a385edd9257a2cd60238927918a8eb07edf4
songhee24 <akaratel61@gmail.com>
2023-07-10 22:02:37 +0600

    video player #2

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -7,9 +7,34 @@ import { meal } from "../../constants";
 const Intro = () => {
   const [playVideo, setPlayVideo] = useState(false);
   const videoRef = useRef();
+
+  const onHandleVideo = () => {
+    setPlayVideo((prevState) => !prevState);
+  };
+
+  if (playVideo) {
+    videoRef.current?.pause();
+  } else {
+    videoRef.current?.play();
+  }
+
   return (
     <div className="app__video">
-      <video ref={videoRef} src={meal} type="video/mp4" loop controls={false} />
+      <video
+        ref={videoRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted={false}
+      />
+
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={onHandleVideo}
+        ></div>
+      </div>
     </div>
   );
 };

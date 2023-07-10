61e453a73bfdaf987d406ba757b8111e5f8d289d
songhee24 <akaratel61@gmail.com>
2023-07-10 21:57:56 +0600

    video player

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 
-import './Intro.css';
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = useRef();
+  return (
+    <div className="app__video">
+      <video ref={videoRef} src={meal} type="video/mp4" loop controls={false} />
+    </div>
+  );
+};
 
 export default Intro;

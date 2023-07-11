abe5889cecc0ccee327e7538a59e3c8fe8003752
songhee24 <akaratel61@gmail.com>
2023-07-11 22:31:57 +0600

    laurels section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './Laurels.css';
+import "./Laurels.css";
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+    </div>
+
+    <div className="app__wrapper_img"></div>
   </div>
 );
 

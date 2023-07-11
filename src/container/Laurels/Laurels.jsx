9575191dbeb860f4cf45846eebdbd8392c991581
songhee24 <akaratel61@gmail.com>
2023-07-11 22:38:16 +0600

    AwardCard logic

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -4,11 +4,33 @@ import "./Laurels.css";
 import { SubHeading } from "../../components";
 import { images, data } from "../../constants";
 
+const AwardCard = ({ award: { title, imgUrl, subtitle } }) => {
+  return (
+    <div className="app__laurels_awards-card">
+      <img src={imgUrl} alt="award" />
+      <div className="app__laurels_awards-card_content">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {subtitle}
+        </p>
+      </div>
+    </div>
+  );
+};
+
 const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
     <div className="app__wrapper_info">
       <SubHeading title="Awards & recognition" />
       <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
     </div>
 
     <div className="app__wrapper_img"></div>

d5fddc87be277f81925be41c186394428eec0e7e
songhee24 <akaratel61@gmail.com>
2023-07-12 01:05:09 +0600

    FindUs section #2

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -6,7 +6,15 @@ import { images } from "../../constants";
 const FindUs = () => {
   return (
     <div className="app__bg app__wrapper section__padding" id="contact">
-      <div className="app__wrapper_info"></div>
+      <div className="app__wrapper_info">
+        <SubHeading title={"Contact"} />
+        <h1 className="headtext__cormorant" style={{ marginBottom: "3rem" }}>
+          Fund us
+        </h1>
+        <div className="app__wrapper-content">
+          <p>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+        </div>
+      </div>
 
       <div className="app__wrapper_img">
         <img src={images.findus} alt="findus" />
